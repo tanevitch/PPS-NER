1db267d03666d389f79547f9527c992d2a2af27a
tanevitch <lubatane@gmail.com>
2024-02-11 21:56:36 -0300

    resultados

--- test.py
+++ test.py
@@ -1,28 +1,164 @@
+import pandas as pd
 import spacy
 
 
-nlp= spacy.load("output/model-last")
+nlp= spacy.load("output-merge/model-best")
 
 
-ner = nlp.get_pipe("ner")
-print(ner.labels)
+metricas = {
+    "DIRECCION": {
+        "tp": 0,
+        "fp": 0,
+        "fn": 0,
+        "p": 0.0,
+        "r": 0.0,
+        "f1": 0.0,
+        "error": [
+            
+        ]
+    },
+    "FOT": {
+        "tp": 0,
+        "fp": 0,
+        "fn": 0,
+        "p": 0.0,
+        "r": 0.0,
+        "f1": 0.0,
+        "error": [
+            
+        ]
+    },
+    "IRREGULAR": {
+        "tp": 0,
+        "fp": 0,
+        "fn": 0,
+        "p": 0.0,
+        "r": 0.0,
+        "f1": 0.0,
+        "error": [
+            
+        ]
+    },
+    "DIMENSIONES": {
+        "tp": 0,
+        "fp": 0,
+        "fn": 0,
+        "p": 0.0,
+        "r": 0.0,
+        "f1": 0.0,
+        "error": [
+            
+        ]
+    },
+    "ESQUINA": {
+        "tp": 0,
+        "fp": 0,
+        "fn": 0,
+        "p": 0.0,
+        "r": 0.0,
+        "f1": 0.0,
+        "error": [
+            
+        ]
+    },
+    "NOMBRE_BARRIO": {
+        "tp": 0,
+        "fp": 0,
+        "fn": 0,
+        "p": 0.0,
+        "r": 0.0,
+        "f1": 0.0,
+        "error": [
+            
+        ]
+    },
+    "CANT_FRENTES": {
+        "tp": 0,
+        "fp": 0,
+        "fn": 0,
+        "p": 0.0,
+        "r": 0.0,
+        "f1": 0.0,
+        "error": [
+            
+        ]
+    },
+    "PILETA": {
+        "tp": 0,
+        "fp": 0,
+        "fn": 0,
+        "p": 0.0,
+        "r": 0.0,
+        "f1": 0.0,
+        "error": [
+            
+        ]
+    }
+}
 
+input = pd.read_csv('ground_truth_75.csv', sep = '|')
+input = input.fillna("")
 
-text= "Casa de 4 ambientes en Lomas del Mirador.  Ubicada en Melo 836, Lomas del Mirador.  Lote de 8,66 x 23 Vivienda con 100 años de antigüedad aprox. (ideal modernizar o demoler) de 90 m2 Cubiertos  o Cocina. o Comedor. o Baño. o 3 Dormitorios. o Patio. o Terraza. o Galpón.  ZONIFICACION Zona: U2B FOS 0,6 FOT 2.25 Densidad 700 Altura Máxima 13 mts  Consultanos para coordinar una visita LUCERO Propiedades  - KP255930 - KPD110810 - -   Publicado vía KiteProp CRM Inmobiliario"
-doc=nlp(text)
-for ent in doc.ents:
-   print(ent.text,ent.label_)
+for index, row in input.iterrows():
+   respuestas= {
+       "DIRECCION": "",
+       "FOT": "",
+       "IRREGULAR":"",
+       "DIMENSIONES": "",
+       "ESQUINA": "",
+       "NOMBRE_BARRIO":"",
+       "CANT_FRENTES": "",
+       "PILETA": ""
+   }
+   doc=nlp(row['descripcion'])
+   for ent in doc.ents:
+      respuestas[ent.label_]+= ent.text+" "
+   for respuesta, esperada, key_metrica in zip(respuestas, list(row[1:]), metricas):
+        if respuesta == "" and esperada == "":
+            metricas[key_metrica]["tn"]+=1
+        else:
+            if key_metrica in ["CANT_FRENTES","DIMENSIONES", "DIRECCION", "FOT", "NOMBRE_BARRIO"]:
+                correcta= nlp(respuestas[key_metrica]).similarity(nlp(esperada)) > 0.93
+            elif key_metrica in [ "ESQUINA", "IRREGULAR", "PILETA"]:
+                correcta= True if esperada==respuestas[key_metrica] or (respuestas[key_metrica] != "" and esperada == True) else False
+            
+            if correcta:
+                metricas[key_metrica]["tp"]+=1
+            else:
+                metricas[key_metrica]["error"].append({
+                    "contexto": row["descripcion"],
+                    "respuesta_predicha": respuestas[key_metrica],
+                    "respuesta_esperada": esperada
+                })
+                if respuestas[key_metrica] == "" and esperada != "":
+                    metricas[key_metrica]["fn"]+=1
+                elif (esperada == "" and respuestas[key_metrica] != ""):
+                    metricas[key_metrica]["fp"]+=1
 
+for metrica, valores in metricas.items():
+    tp = valores["tp"]
+    fp = valores["fp"]
+    fn = valores["fn"]
 
-text= """Amplio PH de tres ambientes. Se encuentra ubicado en calle 5 entre 528 bis y 529.
-"""
-doc=nlp(text)
-for ent in doc.ents:
-   print(ent.text,ent.label_)
+    if (tp + fp) > 0:
+        precision = tp / (tp + fp)
+    else:
+        precision = 0.0
 
+    if (tp + fn) > 0:
+        recall = tp / (tp + fn)
+    else:
+        recall = 0.0
 
-   
-text= "calle 637 entre 137 y 143. Lote de 27m x 150m, haciendo un total de 4050 m2. Barrio cerrado ya conformado compuesto por 9 lotes de diversas medidas. Su destino es para vivienda familiar. Cuenta con los servicios de luz y agua."
-doc=nlp(text)
-for ent in doc.ents:
-   print(ent.text,ent.label_)
+    if (precision + recall) > 0:
+        f1_score = 2 * ((precision * recall) / (precision + recall))
+    else:
+        f1_score = 0.0
+
+    metricas[metrica]["p"] = precision
+    metricas[metrica]["r"] = recall
+    metricas[metrica]["f1"] = f1_score
+
+import json
+with open('resultados.json', 'w', encoding="utf8") as fp:
+    json.dump(metricas, fp, ensure_ascii=False)
