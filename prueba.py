c90cfbc2d055ed881b913ed19dec1a92d01d3ba2
tanevitch <lubatane@gmail.com>
2024-04-05 11:19:51 -0300

    GT 100

--- prueba.py
+++ prueba.py
@@ -1,13 +1,10 @@
 import re
 import spacy
 
-numeros = re.findall(r'\b\d+(?:[.,]\d+)?\b', "LOTE CON MEJORAS (CASA A REFACCIONAR) 8,77 X 26,23 X 8,77 X 26,07")
-numeros_sin_duplicados = set(numeros)
-len(numeros_sin_duplicados)
 
 nlp= spacy.load("output-merge/model-best")
 
-text = "Casa de 4 ambientes en Lomas del Mirador.  Ubicada en Melo 836, Lomas del Mirador.  Lote de 8,66 x 23 Vivienda con 100 años de antigüedad aprox. (ideal modernizar o demoler) de 90 m2 Cubiertos  o Cocina. o Comedor. o Baño. o 3 Dormitorios. o Patio. o Terraza. o Galpón.  ZONIFICACION Zona: U2B FOS 0,6 FOT 2.25 Densidad 700 Altura Máxima 13 mts"
+text = "Lote con mejoras (casa a refaccionar) 8.77 x 26.23 + martillo, sup. total 226 mts2. Todos los servicios. FOS 0.6. FOT 1.6. Zonificación RTMA. Escritura a cargo del comprador."
 
 ents= nlp(text).ents
 print(set([ent.text for ent in ents]))
